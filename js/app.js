0cd67eb3c6d9ff4e9621289b89cb9605330fa607
HazelHsieh <owo0726@gmail.com>
2022-02-13 15:57:28 +0800

    menu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', function(){
-    this.classList.toggle('is-active');
-    menu.classList.toggle('is-active');
 
+hamburger.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    this.classList.toggle('is-active');
+
+    menu.addEventListener('click', function(){
+        menu.classList.remove('is-active');
+        hamburger.classList.remove('is-active');
+    
+    });
 });
